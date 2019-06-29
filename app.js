34c76e2b6fcae7c884f3870a86ec764d62f3a8bd
arjun225 <35868507+arjun225@users.noreply.github.com>
2019-06-29 17:00:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const hostname = '192.168.0.31';
-const port = 4200;
+const port = 42001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n hi this mallikarjuna \n i am working has devops in nodia\n i ahve total 4 years exp in devops \n');
+  res.end('Hello World\n hi this mallikarjuna \n i am working has devops in nodia\n i ahve total 4 years exp in devops \n i created new');
 });
 
 server.listen(port, hostname, () => {
